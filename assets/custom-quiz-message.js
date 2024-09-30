be427a8a7c239a387a358ca2aab569a46ad3c7db
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2024-09-30 20:18:28 -0300

    Initial commit

--- assets/custom-quiz-message.js
+++ assets/custom-quiz-message.js
@@ -6,8 +6,10 @@ document.addEventListener("DOMContentLoaded", function() {
         // Crea el contenedor del mensaje
         var messageDiv = document.createElement('div');
         messageDiv.className = 'custom-quiz-message';
+        messageDiv.id = 'quiz-start-message';  // Asignar ID al contenedor del mensaje
         messageDiv.innerHTML = `
-            <p>Estás a punto de iniciar la evaluación del curso <strong>${quizData.courseName}</strong>, que consta de 30 preguntas. Esta evaluación es contrarreloj, tendrás 45 segundos para responder cada pregunta. Al finalizar, te entregaremos tu resultado, y podrás decidir si quieres publicarlo en el ranking público o mantenerlo privado. Recuerda que solo puedes rendir esta evaluación 3 veces, por lo que te sugerimos prestar mucha atención y responder conscientemente. Cuando estés listo, haz clic en <strong>Comenzar</strong>. ¡Éxito!</p>
+            <a id="back-to-course-link" href="${document.referrer}" class="back-to-course-link">Volver al curso</a>
+            <p id="quiz-start-paragraph">Estás a punto de iniciar la evaluación del curso <strong>${quizData.courseName}</strong>, que consta de 30 preguntas. Esta evaluación es contrarreloj, tendrás 45 segundos para responder cada pregunta. Al finalizar, te entregaremos tu resultado, y podrás decidir si quieres publicarlo en el ranking público o mantenerlo privado. Recuerda que solo puedes rendir esta evaluación 3 veces, por lo que te sugerimos prestar mucha atención y responder conscientemente. Cuando estés listo, haz clic en <strong>Comenzar</strong>. ¡Éxito!</p>
         `;
 
         // Inserta el mensaje antes del botón "Start Quiz"
